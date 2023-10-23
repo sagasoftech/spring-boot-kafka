79b15faa5b2ccca61bae7b983de138fbd2324e31
Sagar Dighe <sagasoftech@gmail.com>
2023-10-23 00:03:01 -0500

    Initial version

---
+++ src/main/java/com/sagasoftech/spring/kafka/SpringBootKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.sagasoftech.spring.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaApplication.class, args);
+	}
+
+}
